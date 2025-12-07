2a775ac49c52228713b44bb90c574b360c2e45f3
Vishal Raj <vraj77245@gmail.com>
2025-12-07 17:47:43 +0530

    Assignment done

---
+++ math.py
@@ -0,0 +1,14 @@
+# Task 1
+num1 = int(input("Please enter first number: \n"))
+num2 = int(input("Please enter second number: \n"))
+
+addition = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+divi = num1 / num2
+
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {multi}")
+print(f"Division: {divi}")
---
+++ message.py
@@ -0,0 +1,6 @@
+first = input("Enter your first name: \n")
+last = input("Enter your last name: \n")
+
+message = f"Hello, {first} {last}! Welcome to the python program."
+
+print(message)
